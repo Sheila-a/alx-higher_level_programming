0b602b5833fbb1749feab9d23beebe8e187bb069
Sheila-a <ugwuodo20@gmail.com>
2022-06-06 17:44:04 +0000

    Task 7: Function that adds 2 tuples(2)

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-        # If too long, cut the tuple to the first 2 elements
-            # if too short, extend the tuple to match length 2
-                a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
-                    b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
-                        c = [x + y for x, y in zip(a, b)]
-                            return tuple(c[0:2])
+    b = 0
+    a = 0
+    if len(tuple_a) >= 1:
+        a += tuple_a[0]
+    if len(tuple_a) >= 2:
+        b += tuple_a[1]
+    if len(tuple_b) >= 1:
+        a += tuple_b[0]
+    if len(tuple_b) >= 2:
+        b += tuple_b[1]
+    return a, b
