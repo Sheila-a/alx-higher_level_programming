b3cfdffd53cc29808a70dcc35d02b1a9891ad4f3
Sheila-a <ugwuodo20@gmail.com>
2022-06-06 20:55:48 +0000

    Task 6: Function that prints a matrix of integers(3)

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
+
 def print_matrix_integer(matrix=[[]]):
-    for lis in matrix:
-        for i in range(len(lis)):
-            end = ''
-            if i == len(lis) - 1:
-                end = '\n'
-            print("{:d}".format(lis[i]), end=end)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+        print("")
