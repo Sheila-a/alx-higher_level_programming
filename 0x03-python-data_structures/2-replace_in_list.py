cdb473d4c3adbebf3518134a8eabc2baab5f010a
Sheila-a <ugwuodo20@gmail.com>
2022-06-06 21:08:48 +0000

    Task 2: Function that replaces an element of a list in spec position (3)

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,8 +2,13 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) -1:
-        return my_list
-    if my_list[idx]:
-        my_list[idx] = element
-    return my_list
+    if idx < 0:
+        return(my_list)
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (my_list)
+
+    my_list[idx] = element
+
+    return (my_list)
