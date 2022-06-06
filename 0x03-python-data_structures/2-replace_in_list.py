e47f1d69506aceb895afdee0eb09fa65a14c23c6
Sheila-a <ugwuodo20@gmail.com>
2022-06-06 21:02:24 +0000

    Task 2: Function that replaces an element..

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,8 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):
+    if idx < 0 or idx > len(my_list) -1:
+        return my_list
+    if my_list[idx]:
         my_list[idx] = element
     return my_list                              
