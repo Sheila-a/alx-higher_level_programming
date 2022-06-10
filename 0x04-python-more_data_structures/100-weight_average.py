d4c6ab0b77e582333f467eca205a58ee2cd58d64
Sheila-a <ugwuodo20@gmail.com>
2022-06-10 03:47:13 +0000

    Advanced 13: Fxn that returns weighted average of all int tuple(2)

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-        if not my_list:
-                    return 0
+        if type(my_list) != list:
+                    return
+                    numer = 0
+                        denom = 0
 
-                    num = 0                
-                        den = 0
-
-                            for tup in my_list:
-                                        num += tup[0] * tup[1]
-                                                den += tup[1]
-                                                         
-                                                             return (num / den)
+                            for i in my_list:
+                                        numer += i[0] * i[1]
+                                                denom += i[1]
+                                                    if denom == 0:
+                                                                return 0
+                                                                return numer / denom
