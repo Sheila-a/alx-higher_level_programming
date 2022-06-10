98897d883d55895f2c4c4e6562510e0dba857ff4
Sheila-a <ugwuodo20@gmail.com>
2022-06-10 03:25:03 +0000

    Task 10: Fxn that returns a list with values multiplied by a number (without loops)(2)

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if type(a_dictionary) == dict and len(a_dictionary) > 0:
-        max = 0
-        idx = 0
-        for i in a_dictionary.keys():
-            if idx == 0:
-                max = i
-                idx +=1
-            if a_dictionary[i] > a_dictionary[max]:
-                max = i
-        return max
+    if not a_dictionary:
+        return (None)
+    return (max(a_dictionary, key=a_dictionary.get))
