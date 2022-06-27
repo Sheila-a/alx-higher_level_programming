391f13c7fefd71a26e0e8ef64286d64944bd2e40
Sheila-a <ugwuodo20@gmail.com>
2022-06-27 16:18:46 +0100

    Task 12: INTERVIEW (Function that converts roman numeral to integer) (2)

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,6 +10,7 @@ def to_subtract(list_num):
 
     return (max_list - to_sub)
 
+
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
@@ -18,11 +19,10 @@ def roman_to_int(roman_string):
         return 0
 
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
     list_keys = list(rom_n.keys())
+
     num = 0
     last_rom = 0
-
     list_num = [0]
 
     for ch in roman_string:
@@ -33,7 +33,9 @@ def roman_to_int(roman_string):
                     list_num = [rom_n.get(ch)]
                 else:
                     list_num.append(rom_n.get(ch))
-                    last_rom = rom_n.get(ch)
-                    num += to_subtract(list_num)
 
-return (num)
+                last_rom = rom_n.get(ch)
+
+    num += to_subtract(list_num)
+
+    return (num)
