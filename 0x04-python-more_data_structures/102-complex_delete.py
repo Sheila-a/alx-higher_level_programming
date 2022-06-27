9197e0e29f0925ec266646011d1da0ff0e29dce6
Sheila-a <ugwuodo20@gmail.com>
2022-06-27 16:27:11 +0100

    Advanced 15: Fxn that deletes keys with a specific value in a dictionary (2)

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
-        if type(a_dictionary) != dict:
-                    return
-                    lis = []
-                        for i in a_dictionary.keys():
-                                    if a_dictionary[i] == value:
-                                                    lis.append(i)
-                                                        for i in lis:
-                                                                    del a_dictionary[i]
-                                                                        return a_dictionary
+    list_keys = list(a_dictionary.keys())
+
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
+
+    return (a_dictionary)
